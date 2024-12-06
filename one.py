46d5e49b220a97babf05e816282b60ad0c181521
tinaia <tinaia@ulrik.uio.no>
2024-12-06 22:56:17 +0100

    Adding solution to day 1 part b of AoC

--- one.py
+++ one.py
@@ -1,21 +1,22 @@
 # Advent of Code - day 1.
-# Task: Compare two and two numbers from two lists, from smallest to biggest, and calculate their difference. In the end,
+# Task a: Compare two and two numbers from two lists, from smallest to biggest, and calculate their difference. In the end,
 # calculate the total difference.
+# Task b: Calculate how many times each nr in list 1 occur in list 2, and find the total similarity score.
 
 class IdNumbers:
     def __init__(self):
         self.list1 = []
         self.list2 = []
-        self.sum = 0
+        self.tot_distance = 0
+        self.tot_score = 0
 
     def read_input(self, file):
-        ids = open(file, "r")
-        for line in ids:
-            # Input-file looks like this: 11  12, so we split on whitespace, and put them in two lists
-            divided = line.split()
-            self.list1.append(int(divided[0]))
-            self.list2.append(int(divided[1]))
-        ids.close()
+        with open(file, "r") as ids:
+            for line in ids:
+                # Input-file looks like this: 11  12, so we split on whitespace, and put them in two lists
+                divided = line.split()
+                self.list1.append(int(divided[0]))
+                self.list2.append(int(divided[1]))
 
     def calc_distances(self):
         # Sort lists from smallest to biggest
@@ -24,17 +25,34 @@ class IdNumbers:
         for nr in range(0, len(self.list1)):
             diff = abs(self.list1[nr] - self.list2[nr])
             # Add the difference to the total sum
-            self.sum += diff
+            self.tot_distance += diff
 
     def get_tot_distance(self):
-        print("Total distance: ", self.sum)
+        return f"Total distance: {self.tot_distance}"
+
+    def calc_similarity_score(self):
+        for nr in self.list1:
+            # Count how many times each nr in list1 occur in list2. Add the product to total score
+            amount = self.list2.count(nr)
+            self.tot_score += amount * nr
+
+    def get_similarity_score(self):
+        return f"The similarity score is: {self.tot_score}"
 
 
-def calc_all_distances():
+def distances():
     numbers = IdNumbers()
     numbers.read_input("input1.txt")
     numbers.calc_distances()
-    numbers.get_tot_distance()
+    print(numbers.get_tot_distance())
 
 
-calc_all_distances()
+def similarity_score():
+    numbers = IdNumbers()
+    numbers.read_input("input1.txt")
+    numbers.calc_similarity_score()
+    print(numbers.get_similarity_score())
+
+
+distances()
+similarity_score()
